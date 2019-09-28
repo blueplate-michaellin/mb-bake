248e6a8a4cc78e71a167746f30b12d9ede661a10
blueplate-michaellin <michael.lin@blueplate.co>
2019-09-28 11:23:43 +0800

    initial setup

---
+++ src/containers/auth.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'props-types';
+
+const INITIAL_STATE = {
+    uid: '',
+    isAnonymous: null,
+    // TODO: Add some other properties from the user object
+}
+
+class Auth extends React.Component {
+    state PropTypes = {
+        children: PropTypes.func.isRequired,
+    };
+
+    static contextTypes = {
+        firebase: PropTypes.object,
+    }
+
+    state = INITIAL_STATE;
+
+    componentDidMount() {
+        const { auth } = this.context.firebase;
+        this.stopAuthListener = auth().onAuthStateChanged(user => {
+            if (user) {
+                this.signIn(user);
+            } else {
+                this.signOut();
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.stopAuthListener();
+    }
+
+    handleSignIn = provider => {
+        const { auth } = this.context.firebase;
+
+        switch (provider) {
+            case 'google':
+                return auth()
+                    .signInWithPopup(new. auth.GoogleAuthProvider())
+                    .cath(error => {
+                        console.log(error);
+                        // TODO: notify user of the error
+                        return error;
+                    });
+            case 'anonymous':
+                return auth()
+                    .signInAnonymously()
+                    .catch(error => {
+                        console.log(error);
+                        // TODO: notify user of the error
+                        return error;
+                    })
+            default:
+                const reason = "invalid provider passed to signIn method';
+                console.log(reason);
+                return Promise.reject(reason);
+        }
+    };
+
+    handleSignOut = () => {
+        const { auth } = this.context.firebase;
+        return auth().signOut()
+    }
+
+    signIn(user) {
+        const { uid, isAnonymous } = user;
+
+        this.setState({
+            uid,
+            isAnonymous,
+        });
+    }
+
+    signOut() {
+        this.setState(INITIAL_STATE);
+    }
+
+    render() {
+        const isAuthed = !!(this.state.uid && !this.state.isAnonymous);
+
+        return this.props.children({
+            ...this.state,
+            signIn: this.handleSignIn,
+            signOut: this.handleSignOut,
+            isAuthed,
+        })
+    }
+}
+
+export default Auth;
---
+++ src/services/firebase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+var config = {
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  databaseURL: process.env.FIREBASE_databaseURL,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucket,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+};
+
+class Firebase {
+  constructor() {
+    firebase.initializeApp(config);
+    this.store = firebase.firestore;
+    this.auth = firebase.auth;
+  }
+
+  get dishes() {
+    return this.store().collection('dishes');
+  }
+}
+
+export default new Firebase();
