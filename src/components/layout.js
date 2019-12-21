b467f3d7b19f7817f9668af2f55b2c44daa2059a
Michael Lin <michael.lin@blueplate.co>
2019-12-21 21:43:34 +0800

    minor fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,10 +36,12 @@ const Layout = ({ children }) => {
             position: "right", // Position may be 'right' or 'left'
             order: "facebook,whatsapp", // Order of buttons
         };
-        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        if (typeof (window) !== "undefined") {
+          var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+          var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+          s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+          var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        }
     };
 
   return (
