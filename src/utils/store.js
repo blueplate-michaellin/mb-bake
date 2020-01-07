c366d8336a4fdf29cc4111eca070c62187811b22
Michael Lin <michael.lin@blueplate.co>
2020-01-07 20:55:42 +0800

    bug fixes

--- src/utils/store.js
+++ src/utils/store.js
@@ -6,20 +6,20 @@ const store = createContext(initialState);
 const { Provider } = store;
 
 const StateProvider = ( { children } ) => {
-    const category = useCategory()
-  const [state, dispatch] = useReducer((state, action) => {
-    switch(action.type) {
-        case category[0]:
-            return category[0];
-        case category[1]:
-            return category[1];
-        case category[2]:
-            return category[2];
-        default:
-            throw new Error()
-    };
-  }, initialState);
 
+    const category = useCategory()
+    const [state, dispatch] = useReducer((state, action) => {
+        switch(action.type) {
+            case category[0]:
+                return category[0];
+            case category[1]:
+                return category[1];
+            case category[2]:
+                return category[2];
+            default:
+                throw new Error()
+        };
+    }, initialState);
   return <Provider value={{ state, dispatch }}>{children}</Provider>;
 };
 
